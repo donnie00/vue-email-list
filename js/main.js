bbe59bc8aa9aa2fe8fe405178672172e2201a065
ippoRulez <ippolitodonato@gmail.com>
2022-11-30 17:56:45 +0100

    added a table for displaying and a button to get new emails

--- js/main.js
+++ js/main.js
@@ -10,24 +10,32 @@ createApp({
 	},
 	//inserisci qui le tue funzioni
 	methods: {
-		fetchMail() {
+		fetchMails() {
+			this.fetchedMail = [];
+
 			axios
 				.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then((mail) => {
 					this.fetchedMail.push(mail.data.response);
-					console.log(this.fetchedMail.length);
-					console.log(this.mailList);
 
 					if (this.fetchedMail.length === 10) {
 						this.mailList = this.fetchedMail;
 					}
 				});
 		},
+
+		printMails() {
+			for (let i = 0; i < 10; i++) {
+				this.fetchMails();
+			}
+		},
+
+		copyMail(text) {
+			navigator.clipboard.writeText(text);
+		},
 	},
 
 	mounted() {
-		for (let i = 0; i < 10; i++) {
-			this.fetchMail();
-		}
+		this.printMails();
 	},
 }).mount('#app');
